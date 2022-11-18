3196d6517bc7b3d506bab4f86fb159d3f966f4b3
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-11-18 13:45:21 +0700

    add redirect condition

--- popup.js
+++ popup.js
@@ -8,14 +8,24 @@ chrome.tabs.getSelected(null, (tab) => {
         open(`https://github.com/PingHuskar`, "_blank")
         open(`https://github.com/PingHuskar?tab=repositories`, "_blank")
     }
-    const [full, org, path] = tablink.match(/https:\/\/(.+).udemy.com\/course\/(.*?)\//)
+    
+    else if (/https:\/\/(.+).udemy.com\/course\/(.*?)\//.test(tablink)) {
+        
+        const [full, org, path] = tablink.match(/https:\/\/(.+).udemy.com\/course\/(.*?)\//)
 
-    switch (org) {
-        case 'www':
-            open(`https://${myOrg}.udemy.com/${path}`)
-            break
-        default:
-            open(`https://www.udemy.com/${path}`)
-            break
+        switch (org) {
+            case 'www':
+                open(`https://${myOrg}.udemy.com/${path}`)
+                break
+            default:
+                open(`https://www.udemy.com/${path}`)
+                break
+        }
+    }
+
+    else if (/https:\/\/(.+).udemy.com\/join\/login-popup\/\?next=\/course-dashboard-redirect\/%3Fcourse_id%3D(\d+)/.test(tablink)) {
+        const [full, org, courseID] = tablink.match(/https:\/\/(.+).udemy.com\/join\/login-popup\/\?next=\/course-dashboard-redirect\/%3Fcourse_id%3D(\d+)/)
+        open(`https://www.udemy.com/${courseID}`)
+        open(`https://${myOrg}.udemy.com/${courseID}`)
     }
 })
